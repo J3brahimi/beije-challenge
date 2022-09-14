b064d218ae10ec06ee7570078f8f1b6efbff1066
Javad Ebrahimi <mje0098@gmail.com>
2022-09-14 13:40:07 +0430

    feat: add and config material-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from "next/head";
+import type { AppProps } from "next/app";
+
+// Material-UI
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { mainTheme } from "theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <ThemeProvider theme={mainTheme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ theme/mainTheme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles";
+
+// Create a theme instance.
+const mainTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#CEB7A1",
+    },
+    secondary: {
+      main: "#EFE7DD",
+    },
+  },
+});
+
+export default mainTheme;
