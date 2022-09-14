72fdec7cdadc14d7c23c323a2fcb651e6af409bb
Javad Ebrahimi <mje0098@gmail.com>
2022-09-14 13:45:50 +0430

    feat: add website main title, description, favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,22 @@
 import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Head>
+        <title>BEIJE</title>
+        <meta
+          name="description"
+          content="BEIJE is a subscription service for menstrual hygiene products."
+        />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+
+      {/* <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> */}
+    </div>
+  );
 };
 
 export default Home;
